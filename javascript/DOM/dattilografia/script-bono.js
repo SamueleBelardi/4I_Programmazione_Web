c0e0d8a96ebb14eba260d569507a13abe6e1eb26
Samuele Belardi <samuelebelardi@D252x26Mac.local>
2026-05-21 07:49:45 +0200

    completato esercizio dattilografia

--- javascript/DOM/dattilografia/script-bono.js
+++ javascript/DOM/dattilografia/script-bono.js
@@ -1,5 +1,5 @@
-"use strict"
-
+"use strict";
+let dimensioniBarra = 100;
 let contatore = 0;
 const sezionePunti = document.getElementById("punti");
 sezionePunti.innerText = contatore;
@@ -7,24 +7,23 @@ let tastoAcceso = undefined;
 
 function creaPagina() {
     const alfabeto = "abcdefghijklmnopqrstuvwxyz";
-    const padre = document.getElementById("plancia")
-
+    const padre = document.getElementById("plancia");
     alfabeto.split("").forEach(lettera => {
         const nuovo = document.createElement("span");
         nuovo.innerText = lettera;
-        padre.appendChild(nuovo)
+        padre.appendChild(nuovo);
     });
-
-    const intervallo = setInterval(cambiaTasto, 3000);
+    cambiaTasto();
+    setInterval(cambiaTasto, 3000);
+    setInterval(barraCaricamento, 100);
+    document.addEventListener("keydown", aumentaPunteggio);
 }
 
 function cambiaTasto() {
     const tasti = document.querySelectorAll("#plancia span");
-
     if (tastoAcceso != undefined) {
         tastoAcceso.classList.remove("colorato");
     }
-
     const numero = Math.floor(Math.random() * 26);
     let scelto = tasti[numero];
     scelto.classList.add("colorato");
@@ -32,11 +31,22 @@ function cambiaTasto() {
 }
 
 function aumentaPunteggio(e) {
-    const accesso = tastoAcceso.innerText;
     if (tastoAcceso != undefined) {
+        const accesso = tastoAcceso.innerText;
         if (e.key === accesso) {
             contatore++;
+            sezionePunti.innerText = contatore;
         }
     }
+}
 
+function barraCaricamento() {
+    const barra = document.getElementById("barra-caricamento");
+
+    if (dimensioniBarra === 0) {
+        dimensioniBarra = 100;
+    }
+    dimensioniBarra -= 1;
+
+    barra.style.width = dimensioniBarra + "%"
 }
